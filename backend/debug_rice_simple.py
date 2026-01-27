1aa71649124cb33e41f2aeb50151798a08c9972a
Aananda Bastola <bastola.aananda24@gmail.com>
2026-01-27 20:34:04 +0545

    Cleanup unused backend scripts

--- backend/debug_rice_simple.py
+++
@@ -1,9 +0,0 @@
-import torch
-import sys
-
-try:
-    torch.load('models/kisanai_rice_complete.pth', map_location='cpu', weights_only=False)
-except Exception as e:
-    with open('error_msg.txt', 'w') as f:
-        f.write(str(e))
-    print("Error written to file")
